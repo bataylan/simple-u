df3811f5635655f852d2030ef01f1f08af94aebd
Baran Taylan <dev.barantaylan@gmail.com>
2024-03-16 22:46:43 +0300

    Add two d collider helpers wip

---
+++ Collider/ColliderWrappers.cs
@@ -0,0 +1,224 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleU.TWOD.Collider
+{
+    public class TriggerEnhancedHandler : MonoBehaviour
+    {
+        public Action<Collider2D> handleTriggerEnter;
+        public Action<Collider2D> handleTriggerExit;
+
+        public void Init(Action<Collider2D> triggerEnter,
+            Action<Collider2D> triggerExit)
+        {
+            if (triggerEnter != null)
+                handleTriggerEnter += triggerEnter;
+            if (triggerExit != null)
+                handleTriggerExit += triggerExit;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (handleTriggerEnter != null)
+                handleTriggerEnter.Invoke(other);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (handleTriggerExit != null)
+                handleTriggerExit.Invoke(other);
+        }
+    }
+
+    public class TriggerEnhancedHandler<T>
+        where T : MonoBehaviour, ITrackDeathOwner<T>
+    {
+        public Action<T> handleTriggerEnter;
+        public Action<T> handleTriggerExit;
+        public Action<T> onFirstItemEntered;
+        public Action<T> onLastItemLeft;
+        public string targetTag;
+
+        public List<T> ItemList => _itemList;
+        private List<T> _itemList = new List<T>();
+        private TriggerEnhancedHandler _triggerHandler;
+
+        public TriggerEnhancedHandler(TriggerEnhancedHandler triggerHandler, string targetTag)
+        {
+            if (triggerHandler == null)
+                return;
+
+            this.targetTag = targetTag;
+            _triggerHandler = triggerHandler;
+            _triggerHandler.handleTriggerEnter += OnTriggerEnter2D;
+            _triggerHandler.handleTriggerExit += OnTriggerExit2D;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.gameObject.CompareTag(targetTag))
+                return;
+
+            var item = other.gameObject.GetComponent<T>();
+            if (item == null)
+            {
+                var colRef = other.gameObject.GetComponent<ColliderReference>();
+                if (colRef == null)
+                    return;
+
+                item = colRef.GetComponentSafe<T>();
+                if (item == default(T))
+                    return;
+            }
+            AddItem(item);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (!other.gameObject.CompareTag(targetTag))
+                return;
+
+            var item = other.gameObject.GetComponent<T>();
+            if (item == null)
+            {
+                var colRef = other.gameObject.GetComponent<ColliderReference>();
+                if (colRef == null)
+                    return;
+
+                item = colRef.GetComponentSafe<T>();
+                if (item == default(T))
+                    return;
+            }
+            RemoveItem(item);
+        }
+
+        private void AddItem(T item)
+        {
+            if (item == null)
+                return;
+
+            if (_itemList.Contains(item))
+                return;
+
+            item.deathSender.RegisterOnDeath(RemoveItem);
+
+            if (_itemList.Count <= 0 && onFirstItemEntered != null)
+                onFirstItemEntered.Invoke(item);
+
+            _itemList.Add(item);
+
+            if (handleTriggerEnter != null)
+                handleTriggerEnter.Invoke(item);
+        }
+
+        private void RemoveItem(T item)
+        {
+            if (item == null)
+                return;
+
+            if (!_itemList.Contains(item))
+                return;
+
+            item.deathSender.UnRegisterOnDeath(RemoveItem);
+
+            _itemList.Remove(item);
+
+            if (_itemList.Count <= 0 && onLastItemLeft != null)
+                onLastItemLeft.Invoke(item);
+
+            if (handleTriggerExit != null)
+                handleTriggerExit.Invoke(item);
+        }
+
+        private void RemoveAll()
+        {
+            if (_itemList == null)
+                return;
+
+            for (int i = 0; i < _itemList.Count; i++)
+            {
+                _itemList[i].deathSender.UnRegisterOnDeath(RemoveItem);
+            }
+
+            _itemList = null;
+        }
+
+        public void Disable()
+        {
+            _triggerHandler.enabled = false;
+        }
+
+        public void ResetActions()
+        {
+            handleTriggerEnter = null;
+            handleTriggerExit = null;
+        }
+
+        public void Remove()
+        {
+            ResetActions();
+            _triggerHandler.handleTriggerEnter -= OnTriggerEnter2D;
+            _triggerHandler.handleTriggerExit -= OnTriggerExit2D;
+            _triggerHandler = null;
+            RemoveAll();
+        }
+    }
+
+    public interface ITrackDeathOwner<T> where T : MonoBehaviour
+    {
+        DeathSender<T> deathSender { get; }
+        void RegisterOnDeath(Action<T> target);
+        void UnRegisterOnDeath(Action<T> target);
+    }
+
+    public class DeathSender<T> where T : MonoBehaviour
+    {
+        private Action<T> _onDeath;
+
+        public void RegisterOnDeath(Action<T> onDeath)
+        {
+            if (onDeath != null)
+                _onDeath += onDeath;
+        }
+
+        public void UnRegisterOnDeath(Action<T> onDeath)
+        {
+            if (onDeath != null)
+                _onDeath -= onDeath;
+        }
+
+        public void InvokeOnDeath(T obj, bool reset = false)
+        {
+            if (_onDeath != null)
+                _onDeath.Invoke(obj);
+
+            if (reset)
+                Reset();
+        }
+
+        public void Reset()
+        {
+            _onDeath = null;
+        }
+    }
+
+    public class ColliderReference : MonoBehaviour
+    {
+        private Transform _refTr;
+
+        public void Init(Transform refTr)
+        {
+            _refTr = refTr;
+        }
+
+        public T GetComponentSafe<T>()
+        {
+            if (!_refTr)
+                return default(T);
+
+            return _refTr.GetComponent<T>();
+        }
+    }
+}
