664461e752bccc4e4c05abdb7afc90f313604207
Baran Taylan <dev.barantaylan@gmail.com>
2024-09-11 23:48:51 +0300

    update item asset for ui relative indexes

--- DataContainers/ItemAsset.cs
+++ DataContainers/ItemAsset.cs
@@ -6,10 +6,13 @@ namespace SimpleU.DataContainer
 {
     public class ItemAsset : ScriptableObject, IItemAsset
     {
-        
+        [SerializeField] private Vector2Int[] relativeSlotIndexes = new Vector2Int[1] { new Vector2Int(0, 0) };
+
+        public Vector2Int[] RelativeSlotIndexes => relativeSlotIndexes;
     }
 
     public interface IItemAsset
     {
+        public Vector2Int[] RelativeSlotIndexes { get; }
     }
 }
